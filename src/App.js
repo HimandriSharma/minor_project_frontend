6c8df17f8e65963bc07dc4eecdb76a6072acde27
himandrishram27 <himandrisharma27@gmail.com>
2021-12-12 23:52:11 +0530

    added gallery image input and finalized UI

--- src/App.js
+++ src/App.js
@@ -1,43 +1,64 @@
 import React, { Fragment, useState } from "react";
-import ReactDOM from "react-dom";
 import { Camera } from "./camera";
+import {FaCameraRetro,FaWindowClose} from 'react-icons/fa'
 import { Root, Preview, Footer, GlobalStyle } from "./styles.js";
+import UploadComponent from "./Upload";
+import "./styles.css";
 
 function App() {
   const [isCameraOpen, setIsCameraOpen] = useState(false);
   const [cardImage, setCardImage] = useState();
-
+  const [state, setState] = useState({
+    upload: {
+      pictures: [],
+      maxFileSize: 5242880,
+      imgExtension: [".jpg", ".png"],
+      defaultImages: [
+      ]
+    }
+  });
+  function handleChange(files) {
+    const { pictures } = state;
+    console.warn({ pictures, files });
+  };
   return (
-    <Fragment>
-      <Root>
-        {isCameraOpen && (
-          <Camera
-            onCapture={blob => setCardImage(blob)}
-            onClear={() => setCardImage(undefined)}
-          />
-        )}
+    <div>
+      <center><h1>Text Recognizer and Translator</h1></center>
+      <UploadComponent
+        {...state.upload}
+        handleChange={handleChange}
+      />
+      <Fragment>
+        <Root>
+          {isCameraOpen && (
+            <Camera
+              onCapture={blob => setCardImage(blob)}
+              onClear={() => setCardImage(undefined)}
+            />
+          )}
 
-        {cardImage && (
-          <div>
-            <h2>Preview</h2>
-            <Preview src={cardImage && URL.createObjectURL(cardImage)} />
-          </div>
-        )}
+          {cardImage && (
+            <div>
+              <h2>Preview</h2>
+              <Preview src={cardImage && URL.createObjectURL(cardImage)} />
+            </div>
+          )}
 
-        <Footer>
-          <button onClick={() => setIsCameraOpen(true)}>Open Camera</button>
-          <button
-            onClick={() => {
-              setIsCameraOpen(false);
-              setCardImage(undefined);
-            }}
-          >
-            Close Camera
-          </button>
-        </Footer>
-      </Root>
-      <GlobalStyle />
-    </Fragment>
+          <Footer>
+            <button onClick={() => setIsCameraOpen(true)}><FaCameraRetro size="40px"/></button>
+            <button
+              onClick={() => {
+                setIsCameraOpen(false);
+                setCardImage(undefined);
+              }}
+            >
+             <FaWindowClose size="40px"/>
+            </button>
+          </Footer>
+        </Root>
+        <GlobalStyle />
+      </Fragment>
+    </div>
   );
 }
 
